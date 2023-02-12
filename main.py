3b9639bd0ce4b427d2437ad9eb29f8d5b8e24551
talhakhalidmtk <87082752+talhakhalidmtk@users.noreply.github.com>
2023-02-13 00:26:11 +0500

    strategy 01 completed

--- main.py
+++ main.py
@@ -26,9 +26,32 @@ def check_conditions(data_frame):
     previous = data_frame.shift(1)
     previous_previous = data_frame.shift(2)
 
-    data_frame['sell'] = (current['high'] >= previous['high']) & (current['low'] > previous['low']) & (current['green'] > current['red']) & (previous['green'] > previous['red']) & (previous_previous['red'] > previous_previous['green'])
-    data_frame['buy'] = (current['high'] < previous['high']) & (current['low'] <= previous['low']) & (current['green'] < current['red']) & (previous['green'] < previous['red']) & (previous_previous['red'] < previous_previous['green'])
-    
+    data_frame['sell'] = (current['high'] >= previous['high']) & (current['low'] > previous['low']) & (current['green'] > current['red']) & (previous['green'] > previous['red']) & (previous_previous['red'] > previous_previous['green']) & (current['close'] < previous['close'])
+    data_frame['buy'] = (current['high'] < previous['high']) & (current['low'] <= previous['low']) & (current['green'] < current['red']) & (previous['green'] < previous['red']) & (previous_previous['red'] < previous_previous['green']) & (current['close'] > previous['close'])
+
+    data_frame['SL'] = 0
+    data_frame['TP'] = 0
+
+    for i in range(len(data_frame)):
+        if data_frame.iloc[i]['sell'] == True:
+            high = data_frame.iloc[i]['high'] + 0.250
+            close = data_frame.iloc[i]['close'] - 0.300
+            sl = data_frame[data_frame['high']==high]
+            if len(sl) > 0:
+                data_frame.at[i,'SL'] = sl.iloc[0]['time']
+            tp = data_frame[data_frame['close']==close]
+            if len(tp) > 0:        
+                data_frame.at[i,'TP'] = tp.iloc[0]['time']
+        elif data_frame.iloc[i]['buy'] == True:
+            high = data_frame.iloc[i]['high'] - 0.250
+            close = data_frame.iloc[i]['close'] + 0.300
+            sl = data_frame[data_frame['high']==high]
+            if len(sl) > 0:
+                data_frame.at[i,'SL'] = sl.iloc[0]['time']
+            tp = data_frame[data_frame['close']==close]
+            if len(tp) > 0:        
+                data_frame.at[i,'TP'] = tp.iloc[0]['time']
+            
     data_frame = data_frame[(data_frame['sell'] == True) | (data_frame['buy'] == True)] 
     
     return data_frame
@@ -59,6 +82,10 @@ def main():
     data_frame = check_conditions(data_frame)
 
     data_frame.to_csv('trade_trigger.csv', index=False)
+
+    print("SL", len(data_frame[data_frame['SL']!=0]))
+    print("TP", len(data_frame[data_frame['TP']!=0]))
+    print("Total", len(data_frame))
 
 
 if __name__ == '__main__':
